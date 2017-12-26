3c78785de878ec70f4dc310e24852cf1ffc6b9be
Amarjeet Rai <jatt27@gmail.com>
2017-12-26 21:16:24 +0000

    Change date format

--- src/containers/WallpaperPage.js
+++ src/containers/WallpaperPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 import Api from '../libs/Api'
 
 class WallpaperPage extends Component {
@@ -33,7 +34,7 @@ class WallpaperPage extends Component {
         <button onClick={() => this.props.history.goBack()}>Back</button>
         <h1 className="title">{this.state.wallpaper.desc}</h1>
         <img className="img-fluid" src={imgSrc} alt={this.state.wallpaper.desc}/>
-        <p>{this.state.wallpaper.date}</p>
+        <p>{this.state.wallpaper.copyright} - {moment(this.state.wallpaper.date, 'YYYYMMDD').format('MMMM Do YYYY')}</p>
       </div>
     )
   }
